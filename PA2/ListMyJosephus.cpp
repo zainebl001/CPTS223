b652b0b15c8fabe879c445acdcb8057493fb686f
zainebl000 <zainlon24@gmail.com>
2025-06-08 22:14:23 -0700

    Completed both List files

--- PA2/ListMyJosephus.cpp
+++ PA2/ListMyJosephus.cpp
@@ -1,1 +1,43 @@
-//Hello World
+#include "ListMyJosephus.hpp"
+#include <iostream>
+
+ListMyJosephus::ListMyJosephus(int m, int n, const std::vector<std::string& cities) : M(m), N(n)
+{
+	for (int i = 0; i < N; i++)
+	{
+		circle.push_back(Destination(i, cities[i]));
+	}
+	cursor = circle.begin();
+}
+ListMyJosephus::~ListMyJosephus() { clear(); }
+
+void ListMyJosephus::clear() { circle.clear(); }
+
+int ListMyJosephus::currentSize const
+{
+	return circle.size();
+}
+
+bool ListMyJosephus::isEmpty() const { return circle.empty(); }
+
+Destination ListMyJosephus::eliminateDestination()
+{
+	for (int i = 0; i < M; ++i)
+	{
+		++cursor;
+		if (cursor == circle.end()) cursor = circle.begin();
+	}
+	Destination eliminated = *cursor;
+	cursor = circle.erase(cursor);
+	if (cursor == circle.end()) cursor =  circle.begin();
+	return eliminated;
+}
+
+void ListMyJosephus::printAllDestinations() const
+{
+	for (const auto& d : circle)
+	{
+		d.printDestinationName();
+	}
+	std::cout << std::endl
+}
--- PA2/ListMyJosephus.hpp
+++ PA2/ListMyJosephus.hpp
@@ -1,1 +1,24 @@
-//Hello World
+#ifndef LISTMYJOSEPHUS_HPP
+#define LISTMYJOSEPHUS_HPP
+#include <list>
+#include <string>
+#include "Destination.hpp"
+
+class ListMyJosephus
+{
+private:
+	int m, n;
+	std::list<Destination> circle;
+	std::list<Destination>::iterator cursor;
+
+public:
+	ListMyJosephus(int m, int n, const std::vector<std::string>& cities);
+	~ListMyJosephus();
+	void clear();
+	int currentSize() const;
+	bool isEmpty() const;
+	Destination eliminateDestination();
+	void printAllDestinations() const;
+};
+
+#endif
