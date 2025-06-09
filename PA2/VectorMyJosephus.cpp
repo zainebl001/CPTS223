ddc04412a9c87d96bb125bb26700425c0251245f
zainebl000 <zainlon24@gmail.com>
2025-06-08 22:53:25 -0700

    Completed both Vector files

--- PA2/VectorMyJosephus.cpp
+++ PA2/VectorMyJosephus.cpp
@@ -1,1 +1,37 @@
-//Hello World
+#include "VectorMyJosephus.hpp"
+#include <iostream>
+
+VectorMyJosephus::VectorMyJosephus(int m, int n, const std::vector<std::string>& cities) : M(m), N(n), index(0)
+{
+	for (int i = 0; i < N; ++i)
+	{
+		circle.push_back(Destination(i, cities[i]));
+	}
+}
+
+VectorMyJosephus::~VectorMyJosephus() { clear(); }
+void VectorMyJosephus::clear()
+{
+	circle.clear();
+}
+int VectorMyJosephus::currentSize() const { return circle.size(); }
+
+bool VectorMyJosephus::isEmpty() const { return circle.empty(); }
+
+Destination VectorMyJosephus::eliminateDestination()
+{
+	index = (index + M) % circle.size();
+	Destination eliminated = circle[index];
+	circle.erase(circle.begin() + index);
+	if (index >= circle.size()) index = 0;
+	return eliminated;
+}
+
+void VectorMyJosephus::printAllDestinations() const
+{
+	for (const auto& d : circle)
+	{
+		d.printDestinationName()l
+	}
+	std::cout << std::endl;
+}
--- PA2/VectorMyJosephus.hpp
+++ PA2/VectorMyJosephus.hpp
@@ -1,1 +1,22 @@
-//Hello World
+#ifndef VECTORMYJOSEPHUS_HPP
+#define VECTORMYJOSEPHUS_HPP
+#include <vector>
+#include "Destination.hpp"
+
+class VectorMyJosephus
+{
+private:
+	int M, N, index;
+	std::vector<Destination> circle;
+
+public:
+	VectorMyJosephus(int m, int n, const std::vector<std::string>& cities);
+	~VectorMyJosephus();
+	void clear();
+	int currentSize() const;
+	bool isEmpty() const;
+	Destination eliminateDestination();
+	void printAllDestinations() const;
+};
+
+#endif
