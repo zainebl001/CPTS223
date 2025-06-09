cff0b73b4418f750b05d880891768905fb6fabb6
zainebl000 <zainlon24@gmail.com>
2025-06-08 19:44:29 -0700

    Completed both destination files

--- PA2/Destination.cpp
+++ PA2/Destination.cpp
@@ -1,1 +1,22 @@
-//Hello World
+#include "Destination.hpp"
+
+Destination::Destination(int pos, std::string city) : position(pos), name(city) {}
+Destination::~Destination()
+{
+}
+
+void Destination::printPosition() const
+{
+	std::cout << position << " ";
+}
+
+void Destination::printDestinationName() const
+{
+	srd::cout << name << " ";
+}
+
+int Destination::getPosition() const { return position; }
+std::string Destination::getName() const
+{
+	return name;
+}
--- PA2/Destination.hpp
+++ PA2/Destination.hpp
@@ -1,1 +1,21 @@
-//Hello Worlda
+#ifndef DESTINATION_HPP
+#define DESTINATION_HPP
+#include <string>
+#include <iostream>
+
+class Destination
+{
+public:
+	Destination(int pos, std::string city);
+	~Destination();
+	void printPosition() const;
+	void printDestinationName() const;
+	int getPosition const;
+	std::string getName() const;
+
+private:
+	int position;
+	std::string name;
+};
+
+#endif
