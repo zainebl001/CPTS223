9940bbd1a421b73aee084f3f45f8c13caa30b72a
zainebl000 <zainlon24@gmail.com>
2025-06-22 21:59:05 -0700

    wrote USCity.hpp

---
+++ PA3/USCity.hpp
@@ -0,0 +1,20 @@
+#ifndef USCITY_HPP
+#define USCITY_HPP
+
+#include <string>
+
+class USCity
+{
+	std::string lat;
+	std::string lng;
+	std::string city;
+	std::string state_id;
+	std::string state_name;
+	std::string zcta;
+	std::string parent_zcta;
+	std::string population;
+	std::string density;
+	std::string timezone;
+};
+
+#endif
