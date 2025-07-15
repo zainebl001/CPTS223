19ff1c7ca1d3becf9f3e211fb9622aa44060b77f
zainebl000 <zainlon24@gmail.com>
2025-07-14 22:08:08 -0700

    Added skelton code to files and created directory

---
+++ PA4/README.md
@@ -0,0 +1,2 @@
+# PA4 Skeleton Code
+We expect a fully functioninig command line REPL application for an inventory querying system. Feel free to modify the skeleton code as you see fit.
---
+++ PA4/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printHelp()
+{
+    cout << "Supported list of commands: " << endl;
+    cout << " 1. find <inventoryid> - Finds if the inventory exists. If exists, prints details. If not, prints 'Inventory not found'." << endl;
+    cout << " 2. listInventory <category_string> - Lists just the id and name of all inventory belonging to the specified category. If the category doesn't exists, prints 'Invalid Category'.\n"
+         << endl;
+}
+
+bool validCommand(string line)
+{
+    return (line == ":help") ||
+           (line.rfind("find", 0) == 0) ||
+           (line.rfind("listInventory") == 0);
+}
+
+void evalCommand(string line)
+{
+    if (line == ":help")
+    {
+        printHelp();
+    }
+    // if line starts with find
+    else if (line.rfind("find", 0) == 0)
+    {
+        // Look up the appropriate datastructure to find if the inventory exist
+        cout << "YET TO IMPLEMENT!" << endl;
+    }
+    // if line starts with listInventory
+    else if (line.rfind("listInventory") == 0)
+    {
+        // Look up the appropriate datastructure to find all inventory belonging to a specific category
+        cout << "YET TO IMPLEMENT!" << endl;
+    }
+}
+
+void bootStrap()
+{
+    cout << "\n Welcome to Amazon Inventory Query System" << endl;
+    cout << " enter :quit to exit. or :help to list supported commands." << endl;
+    cout << "\n> ";
+    // TODO: Do all your bootstrap operations here
+    // example: reading from CSV and initializing the data structures
+    // Don't dump all code into this single function
+    // use proper programming practices
+}
+
+int main(int argc, char const *argv[])
+{
+    string line;
+    bootStrap();
+    while (getline(cin, line) && line != ":quit")
+    {
+        if (validCommand(line))
+        {
+            evalCommand(line);
+        }
+        else
+        {
+            cout << "Command not supported. Enter :help for list of supported commands" << endl;
+        }
+        cout << "> ";
+    }
+    return 0;
+}
